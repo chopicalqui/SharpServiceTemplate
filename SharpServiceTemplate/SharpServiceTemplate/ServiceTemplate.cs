c4393a24c568b04081aa840083c58a3c7379c03d
Lukas Reiter <chopicalqui@protonmail.com>
2020-04-16 16:40:30 +0200

    First commit

---
+++ SharpServiceTemplate/SharpServiceTemplate/ServiceTemplate.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace SharpServiceTemplate
+{
+	public partial class ServiceTemplate : ServiceBase
+	{
+		public ServiceTemplate()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			var processStartInfo = new ProcessStartInfo
+			{
+				FileName = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe",
+				Arguments = @"-Sta -Nop -Window Hidden -EncodedCommand <TODO>"
+			};
+
+			var process = new Process
+			{
+				StartInfo = processStartInfo
+			};
+
+			process.Start();
+			process.WaitForExit();
+			process.Dispose();
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
